95de480d887e1961e5ad186a13b3af8328768458
Cyril Janecek <cyril.janecek.cz@gmail.com>
2022-10-29 14:04:41 +0200

    Use static generation instead for article list

--- pages/article-list.tsx
+++ pages/article-list.tsx
@@ -1,24 +1,12 @@
 import Link from "next/link"
 import axios from "axios"
-import { useState, useEffect } from "react"
 
 const axiosInstance = axios.create({
   baseURL: 'https://fullstack.exercise.applifting.cz',
   headers: { 'X-API-KEY': '950a7a91-9435-4179-b89f-3944c2f128f8' }
 });
 
-export default function ArticleList() {
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    async function getArticles() {
-      const response = await axiosInstance.get('/articles');
-      console.log(response.data.items);
-      setArticles(response.data.items)
-    }
-    getArticles();
-  }, [])
-
+export default function ArticleList({articles}) {
   return (
     <>
       <h1>Article list</h1>
@@ -26,4 +14,13 @@ export default function ArticleList() {
       <h2><Link href="/">Go home</Link></h2>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const response = await axiosInstance.get('/articles');
+  return {
+    props: {
+      articles: response.data.items,
+    },
+  }
 }
