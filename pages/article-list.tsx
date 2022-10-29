d6d053ff4085df0502bb57fe4b00db653cb591c8
Cyril Janecek <cyril.janecek.cz@gmail.com>
2022-10-29 11:40:58 +0200

    Implement basic article fetching with axios

--- pages/article-list.tsx
+++ pages/article-list.tsx
@@ -1,10 +1,28 @@
 import Link from "next/link"
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+const axiosInstance = axios.create({
+  baseURL: 'https://fullstack.exercise.applifting.cz',
+  headers: { 'X-API-KEY': '950a7a91-9435-4179-b89f-3944c2f128f8' }
+});
 
 export default function ArticleList() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function getArticles() {
+      const response = await axiosInstance.get('/articles');
+      console.log(response.data.items);
+      setArticles(response.data.items)
+    }
+    getArticles();
+  }, [])
+
   return (
     <>
       <h1>Article list</h1>
-      <h2><Link href="/article">Go to article</Link></h2>
+      {articles.map(article => <h2 key={article.title}>{article.title}</h2>)}
       <h2><Link href="/">Go home</Link></h2>
     </>
   )
