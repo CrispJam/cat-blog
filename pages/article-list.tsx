8d7282456792c02850138ec5107c70c3d09d1795
Cyril Janecek <cyril.janecek.cz@gmail.com>
2022-10-29 14:34:58 +0200

    Add ArticleType and ArticleListProps

--- pages/article-list.tsx
+++ pages/article-list.tsx
@@ -1,16 +1,28 @@
 import Link from "next/link"
 import axios from "axios"
+import { ArticleType } from "../lib/types";
+
+type ArticleListProp = {
+  articles: Array<ArticleType>;
+}
 
 const axiosInstance = axios.create({
   baseURL: 'https://fullstack.exercise.applifting.cz',
   headers: { 'X-API-KEY': '950a7a91-9435-4179-b89f-3944c2f128f8' }
 });
 
-export default function ArticleList({articles}) {
+export default function ArticleList({articles}: ArticleListProp) {
+  const articleComponents = articles.map(article =>
+    <div key={article.title}>
+      <h2 >{article.title}</h2>
+      <p>{article.perex}</p>
+      <Link href="/">Read more</Link>
+    </div>  
+  )
   return (
     <>
-      <h1>Article list</h1>
-      {articles.map(article => <h2 key={article.title}>{article.title}</h2>)}
+      <h1>Recent articles</h1>
+      {articleComponents}
       <h2><Link href="/">Go home</Link></h2>
     </>
   )
