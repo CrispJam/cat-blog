86e7fe334e904d329d3248b778462ceddbcddf28
Cyril Janecek <cyril.janecek.cz@gmail.com>
2022-11-02 12:12:09 +0100

    Put create page elements in a flexbox

--- pages/articles/create.tsx
+++ pages/articles/create.tsx
@@ -5,7 +5,38 @@ export default function Create() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+
   return (
+    <div className="flex">
+      <div className="flex-1 border-4">
+        Editor
+        <div>
+          <input
+            onChange={e => setTitle(e.target.value)}
+            value={title}
+            placeholder="My first article"
+            className="input input-primary"
+          />
+        </div>
+        <div>
+          <textarea
+            onChange={e => setContent(e.target.value)}
+            value={content}
+            placeholder="Article content written in Markdown"
+            className="textarea textarea-primary"
+          />
+        </div>
+      </div>
+      <div className="flex-1 border-4">
+        Preview
+        <div className="prose">
+          <ReactMarkdown>{content}</ReactMarkdown>
+        </div>
+      </div>
+    </div>
+  )
+
+/*   return (
     <div>
       <h1 className="text-3xl font-bold">Create Article</h1>
       <div>
@@ -26,5 +57,5 @@ export default function Create() {
         <ReactMarkdown>{content}</ReactMarkdown>
       </div>
     </div>
-  )
+  ) */
 }
